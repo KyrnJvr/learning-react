d59623eae426aaa6e1246c522dbfd2711811dc23
Kyle Javier <kyrncjvr@gmail.com>
2025-06-27 15:31:51 +0930

    Implement Todo App functionality with task addition and styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,50 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todos, setTodos] = useState([
+    {id: 1, text: "Sample", completed: false}
+  ]); // List of todos/tasks
+  const [newTodo, setNewTodo] = useState(""); // current input text
+
+  const handleAddTask = () => {
+    if (!newTodo.trim()) return; // avoid empty tasks
+
+    const newItem = {
+      id: Date.now(),
+      text: newTodo,
+      completed: false
+    };
+
+    setTodos([...todos, newItem]); // add new item to the list of tasks/todos
+
+    console.log(todos);
+    setNewTodo(""); // clear user input
+  }
 
   return (
     <>
     <div className="app">
       <h1>Todo App</h1>
+
+      <div className="todo-input">
+        <input 
+          type="text" 
+          placeholder='Enter a task...'
+          value={newTodo}
+          onChange={(e) => setNewTodo(e.target.value)}
+        />
+        <button onClick={handleAddTask}>Add</button>
+      </div>
+
+      {todos.length === 0 ? (
+        <p>No tasks yet.</p>
+      ) : (
+        <ul className='todo-list'>
+        {todos.map((todo) => {
+          return <li key={todo.id}>{todo.text}</li>
+        })}
+      </ul>)}
     </div>
     </>
   )
