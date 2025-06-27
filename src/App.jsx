b0a8e26ab013a778f83dae0a1fe516366df44754
Kyle Javier <kyrncjvr@gmail.com>
2025-06-27 15:42:51 +0930

    include comments on code based on own understanding

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,19 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [todos, setTodos] = useState([
-    {id: 1, text: "Sample", completed: false}
-  ]); // List of todos/tasks
+  const [todos, setTodos] = useState([]); // List of todos/tasks
   const [newTodo, setNewTodo] = useState(""); // current input text
 
   const handleAddTask = () => {
     if (!newTodo.trim()) return; // avoid empty tasks
 
-    const newItem = {
+    const newItem = { // a task object with date as id, text, and boolean completed
       id: Date.now(),
       text: newTodo,
       completed: false
     };
 
     setTodos([...todos, newItem]); // add new item to the list of tasks/todos
-
-    console.log(todos);
     setNewTodo(""); // clear user input
   }
 
@@ -32,7 +28,7 @@ function App() {
           type="text" 
           placeholder='Enter a task...'
           value={newTodo}
-          onChange={(e) => setNewTodo(e.target.value)}
+          onChange={(e) => setNewTodo(e.target.value)} // set the state of the newTodo variable with the value of the input
         />
         <button onClick={handleAddTask}>Add</button>
       </div>
